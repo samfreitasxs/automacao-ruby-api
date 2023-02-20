a22e312d614fe62b233096a1a5f60a2ce641eeb3
samfreitasxs <samuel.sfdf@gmail.com>
2023-02-20 12:57:24 -0300

    feat: implementando print

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -3,9 +3,9 @@ Quando('faço uma requisição GET para o servico Users') do
   end
   
   Entao('o serviço Users deve responder com {int}') do |status_code|
-  expect(@request_users.code).to eq status_code
+     expect(@request_users.code).to eq status_code
   end
   
   Entao('retorna a lista de usuarios') do
-    pending # Write code here that turns the phrase above into concrete actions
+    print @request_users
   end
